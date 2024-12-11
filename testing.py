161643d7d42fd60651aac5cbb5092dbf5cbbdc5e
Nayany664 <nm664@kent.ac.uk>
2024-12-11 02:01:02 +0000

    com

--- testing.py
+++ testing.py
@@ -1,5 +1,5 @@
 
-from main import correct_format #get the class contact_format from the main file
+from main import correct_format #get the class correct_format from the main file
 
 class Testing() :
 
